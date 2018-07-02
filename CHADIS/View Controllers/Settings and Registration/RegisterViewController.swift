ecc2972535eebd8e22daff9880226d87260e5534
paxonyu <paxonyu@gmail.com>
2018-07-02 15:26:43 -0400

    Scrollview
    implemented a scrollview for the register controller

--- CHADIS/View Controllers/Settings and Registration/RegisterViewController.swift
+++ CHADIS/View Controllers/Settings and Registration/RegisterViewController.swift
@@ -10,6 +10,18 @@ import Foundation
 import WebKit
 
 class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var invitationCode: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var rePass: UITextField!
+    @IBOutlet weak var question: UIPickerView!
+    @IBOutlet weak var answer: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Register User"
